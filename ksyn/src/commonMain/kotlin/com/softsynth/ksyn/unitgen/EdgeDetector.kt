53eb5fc706b0f90c58c1a8e3296bcf0d9b2f419f
Phil Burk <philburk@mobileer.com>
2026-02-20 14:22:28 -0800

    convert EdgeDetector unit generator
    It outputs an impulse on rising zero-crossings of its input.

---
+++ ksyn/src/commonMain/kotlin/com/softsynth/ksyn/unitgen/EdgeDetector.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 1997 Phil Burk, Mobileer Inc
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.softsynth.ksyn.unitgen
+
+import com.softsynth.ksyn.Synthesizer
+
+/**
+ * Output 1.0 if the input crosses from zero while rising. Otherwise output zero. The output is a
+ * single sample wide impulse. This can be used with a Latch to implement a "sample and hold"
+ * circuit.
+ *
+ * @author (C) 1997-2010 Phil Burk, Mobileer Inc
+ * @see Latch
+ */
+class EdgeDetector : UnitFilter() {
+    private var previous = 0.0f
+
+    override fun generate() {
+        val inputs = input.getValues()
+        val outputs = output.getValues()
+
+        for (i in 0..<Synthesizer.FRAMES_PER_BLOCK) {
+            val inValue = inputs[i]
+            outputs[i] = if ((previous <= 0.0f) && (inValue > 0.0f)) 1.0f else 0.0f
+            previous = inValue
+        }
+    }
+}
